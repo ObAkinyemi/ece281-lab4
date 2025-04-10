38c40d4b54d5f8ae459dc73b5279314983792a3e
ObAkinyemi <c28obaloluwa.akinyemi@afacademy.af.edu>
2025-04-10 13:56:55 -0600

    Finished top_basys, but synthesis not working. could be dcp file problem?

--- src/top_basys3.vhd
+++ src/top_basys3.vhd
@@ -1,3 +1,4 @@
+
 library ieee;
   use ieee.std_logic_1164.all;
   use ieee.numeric_std.all;
@@ -25,7 +26,7 @@ end top_basys3;
 architecture top_basys3_arch of top_basys3 is
 
     -- signal declarations
-    
+
   
 	-- component declarations
     component sevenseg_decoder is
@@ -67,17 +68,54 @@ architecture top_basys3_arch of top_basys3 is
                 o_clk    : out std_logic		   -- divided (slow) clock
         );
     end component clock_divider;
+    
+    signal w_clk : std_logic;
+    signal fsm_reset : std_logic;
+    signal clk_reset : std_logic;
+    signal w_floor : STD_LOGIC_VECTOR (3 downto 0);  
+    signal w_data : std_logic_vector(3 downto 0);
 	
 begin
 	-- PORT MAPS ----------------------------------------
-    	
+--	port map for seven seg decoder
+	sevenseg_decoder_mappings : sevenseg_decoder port map (
+	   i_Hex => w_floor,
+	   o_seg_n => seg
+	);
 	
+--	port map for the elevator controller
+	elevator_controller_mappings : elevator_controller_fsm port map (
+
+	   i_clk => w_clk,
+	   i_reset => fsm_reset,
+	   go_up_down => sw(1),
+	   is_stopped => sw(0),
+	   o_floor => w_floor
+	);
+	
+    
+
+--  PORT MAP FOR the clock divider
+    clkdiv_inst : clock_divider 		--instantiation of clock_divider to take 
+        generic map ( k_DIV => 25000000 ) -- 1 Hz clock from 100 MHz
+        port map (						  
+            i_clk   => clk,
+            i_reset => clk_reset,
+            o_clk => w_clk
+    );  
+    
 	-- CONCURRENT STATEMENTS ----------------------------
-	
+	fsm_reset <= btnR or btnU;
+	clk_reset <= btnL or btnU;
 	-- LED 15 gets the FSM slow clock signal. The rest are grounded.
-	
+	   led(14) <= '0';
+	   led(15) <= w_clk;
 	-- leave unused switches UNCONNECTED. Ignore any warnings this causes.
 	
 	-- reset signals
+	an(0) <= '0';
+	an(1) <= '1';
+	an(2) <= '1';
+	an(3) <= '1';
 	
 end top_basys3_arch;
