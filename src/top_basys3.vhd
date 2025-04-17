355cae394d9bf7428715f2fba01987d729ccef28
ObAkinyemi <c28obaloluwa.akinyemi@afacademy.af.edu>
2025-04-16 22:10:04 -0600

    Muhammad Metwally carried me through my lab and helped me with part 2. God bless this man.

--- src/top_basys3.vhd
+++ src/top_basys3.vhd
@@ -46,18 +46,18 @@ architecture top_basys3_arch of top_basys3 is
 		 );
 	end component elevator_controller_fsm;
 	
---	component TDM4 is
---		generic ( constant k_WIDTH : natural  := 4); -- bits in input and output
---        Port ( i_clk		: in  STD_LOGIC;
---           i_reset		: in  STD_LOGIC; -- asynchronous
---           i_D3 		: in  STD_LOGIC_VECTOR (k_WIDTH - 1 downto 0);
---		   i_D2 		: in  STD_LOGIC_VECTOR (k_WIDTH - 1 downto 0);
---		   i_D1 		: in  STD_LOGIC_VECTOR (k_WIDTH - 1 downto 0);
---		   i_D0 		: in  STD_LOGIC_VECTOR (k_WIDTH - 1 downto 0);
---		   o_data		: out STD_LOGIC_VECTOR (k_WIDTH - 1 downto 0);
---		   o_sel		: out STD_LOGIC_VECTOR (3 downto 0)	-- selected data line (one-cold)
---	   );
---    end component TDM4;
+	component TDM4 is
+		generic ( constant k_WIDTH : natural  := 4); -- bits in input and output
+        Port ( i_clk		: in  STD_LOGIC;
+               i_reset		: in  STD_LOGIC; -- asynchronous
+               i_D3 		: in  STD_LOGIC_VECTOR (k_WIDTH - 1 downto 0);
+               i_D2 		: in  STD_LOGIC_VECTOR (k_WIDTH - 1 downto 0);
+               i_D1 		: in  STD_LOGIC_VECTOR (k_WIDTH - 1 downto 0);
+               i_D0 		: in  STD_LOGIC_VECTOR (k_WIDTH - 1 downto 0);
+               o_data		: out STD_LOGIC_VECTOR (k_WIDTH - 1 downto 0);
+               o_sel		: out STD_LOGIC_VECTOR (3 downto 0)	-- selected data line (one-cold)
+	   );
+    end component TDM4;
      
 	component clock_divider is
         generic ( constant k_DIV : natural := 2	); -- How many clk cycles until slow clock toggles
@@ -70,16 +70,19 @@ architecture top_basys3_arch of top_basys3 is
     end component clock_divider;
     
     signal w_clk : std_logic;
+    signal w_clk_tdm : std_logic;
     signal fsm_reset : std_logic;
     signal clk_reset : std_logic;
-    signal w_floor : STD_LOGIC_VECTOR (3 downto 0);  
+    signal w_floor_1 : STD_LOGIC_VECTOR (3 downto 0);
+    signal w_floor_2 : std_logic_vector (3 downto 0);
     signal w_data : std_logic_vector(3 downto 0);
+   
 	
 begin
 	-- PORT MAPS ----------------------------------------
 --	port map for seven seg decoder
 	sevenseg_decoder_mappings : sevenseg_decoder port map (
-	   i_Hex => w_floor,
+	   i_Hex => w_data,
 	   o_seg_n => seg
 	);
 	
@@ -90,21 +93,61 @@ begin
 	   i_reset => fsm_reset,
 	   go_up_down => sw(1),
 	   is_stopped => sw(0),
-	   o_floor => w_floor
+	   o_floor => w_floor_1
 	);
 	
-    
+    elevator_controller_mappings2 : elevator_controller_fsm port map (
+
+	   i_clk => w_clk,
+	   i_reset => fsm_reset,
+	   go_up_down => sw(15),
+	   is_stopped => sw(14),
+	   o_floor => w_floor_2
+	);
 
 --  PORT MAP FOR the clock divider
     clkdiv_inst : clock_divider 		--instantiation of clock_divider to take 
-        generic map ( k_DIV => 25000000 ) -- 1 Hz clock from 100 MHz
+        generic map ( k_DIV => 50000000 ) -- 1 Hz clock from 100 MHz
         port map (					    	  
             i_clk   => clk,
             i_reset => clk_reset,
             o_clk => w_clk
-    );  
+    );
     
-	-- CONCURRENT STATEMENTS ----------------------------
+    clkdiv_inst_tdm : clock_divider 		--instantiation of clock_divider to take 
+        generic map ( k_DIV => 10000 ) -- 1 Hz clock from 100 MHz
+        port map (					    	  
+            i_clk   => clk,
+            i_reset => clk_reset,
+            o_clk => w_clk_tdm
+    );
+    
+    TDM_component_mapping : TDM4 port map (
+        i_clk => w_clk_tdm,
+        i_reset => btnU,
+        i_D0 => w_floor_1,
+        i_D1 => x"F",
+        i_D2 => w_floor_2,
+        i_D3 => x"F",
+        o_data => w_data,
+        o_sel => an
+        
+    );
+    
+-- 2 Bit counter Process ----------------------------
+	-- counter rolls over automatically
+	-- asynchronous reset to "00"
+--	twoBitCounter_proc : process(w_clk, fsm_reset)
+--	begin
+--		if fsm_reset = '1' then
+--			f_sel <= "00";
+--		elsif rising_edge(w_clk) then
+--			f_sel <= f_sel + 1;
+--		end if;
+--	end process twoBitCounter_proc;
+	
+	
+--	-- CONCURRENT STATEMENTS ----------------------------
 	fsm_reset <= btnR or btnU;
 	clk_reset <= btnL or btnU;
 	-- LED 15 gets the FSM slow clock signal. The rest are grounded.
@@ -113,9 +156,13 @@ begin
 	-- leave unused switches UNCONNECTED. Ignore any warnings this causes.
 	
 	-- reset signals
-	an(0) <= '0';
-	an(1) <= '1';
-	an(2) <= '1';
-	an(3) <= '1';
-	
+--	w_data <= i_D3 when f_sel = "11" else
+--			  i_D2 when f_sel = "10" else
+--			  i_D1 when f_sel = "01" else
+--			  i_D0;
+			  
+--	w_sel  <=  "0111" when f_sel = "11" else
+--			   "1011" when f_sel = "10" else
+--			   "1101" when f_sel = "01" else
+--			   "1110";	
 end top_basys3_arch;
